9913b32d8ec0f79eafef6112d079eb963ec8e359
Reza Behzadi <reza.behzadi0000@gmail.com>
2023-12-09 02:40:33 -0930

    add style button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -47,3 +47,17 @@ const variations = {
     }
   `,
 };
+
+ const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  border: none;
+  font-weight: 500;
+  border-radius:var(--border-radius-sm);
+  background-color: var(--color-brand-600);
+  color:var(--color-brand-50);
+  box-shadow: var( --shadow-sm);
+  cursor: pointer;
+`;
+
+export default Button
