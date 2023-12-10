f19d4fb354fddbd3b4cae5e2d1a0116d5ecfde89
Reza Behzadi <reza.behzadi0000@gmail.com>
2023-12-10 05:38:10 -0930

    feat:buoldingthe sidebar and main navigation

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,4 +1,10 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import { AiOutlineHome } from "react-icons/ai";
+import { LuCalendarDays } from "react-icons/lu";
+import { HiHomeModern } from "react-icons/hi2";
+import { HiOutlineUsers } from "react-icons/hi2";
+import { IoSettingsOutline } from "react-icons/io5";
 
 const NavList = styled.ul`
   display: flex;
@@ -6,7 +12,7 @@ const NavList = styled.ul`
   gap: 0.8rem;
 `;
 
-const Link = styled.a`
+const StyledNavLink = styled(NavLink)`
   &:link,
   &:visited {
     display: flex;
@@ -26,7 +32,7 @@ const Link = styled.a`
   &.active:link,
   &.active:visited {
     color: var(--color-grey-800);
-    background-color: var(--color-grey-50);
+    background-color: var(--color-grey-100);
     border-radius: var(--border-radius-sm);
   }
 
@@ -44,3 +50,44 @@ const Link = styled.a`
     color: var(--color-brand-600);
   }
 `;
+
+function MainNav() {
+  return (
+    <nav>
+      <NavList>
+        <li>
+          <StyledNavLink to="/dashboard">
+            <AiOutlineHome />
+            <span>Home</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/bookings">
+            <LuCalendarDays />
+            <span>Bookings</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/cabins">
+            <HiHomeModern />
+            <span>Cabins</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/users">
+            <HiOutlineUsers />
+            <span>Users</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/setting">
+            <IoSettingsOutline />
+            <span>Setting</span>
+          </StyledNavLink>
+        </li>
+      </NavList>
+    </nav>
+  );
+}
+
+export default MainNav;
