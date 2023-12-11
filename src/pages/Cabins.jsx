890f952c049c81ba25bdc9a62fb06c3a0d99825c
Reza Behzadi <reza.behzadi0000@gmail.com>
2023-12-11 06:45:52 -0930

    feat:Fetching Cabin Data

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,18 +1,19 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../service/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
 
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  },[])
   return (
+    <>
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
+      <p>Filter / Sort</p>
     </Row>
+    <Row>
+        <CabinTable/>
+    </Row>
+    </>
   );
 }
 
