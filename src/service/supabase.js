ea0bdd4663b2143d4ae832fc730597686d67630d
Reza Behzadi <reza.behzadi0000@gmail.com>
2023-12-11 03:45:07 -0930

    feat:Connecting Supabase With Our React App

---
+++ src/service/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://ctllcbnczuffpdrorcja.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0bGxjYm5jenVmZnBkcm9yY2phIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIyODkwNzQsImV4cCI6MjAxNzg2NTA3NH0.GGZDyI58JOshjtqcHDVUpbKEyYAh8SVIqNt_RwY6msY";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
