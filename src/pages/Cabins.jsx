cfa8c5d8fe425076c3fc26e97eacbfa3ccdb9a29
Reza Behzadi <reza.behzadi0000@gmail.com>
2023-12-15 04:46:05 -0930

    build:intrducing another library react hook form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,13 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import { useState } from "react";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  
+  const [showForm, setShowForm] = useState(false)
 
   return (
     <>
@@ -11,7 +16,10 @@ function Cabins() {
       <p>Filter / Sort</p>
     </Row>
     <Row>
-        <CabinTable/>
+        <CabinTable />
+        
+        <Button onClick={() => setShowForm((show) => !show)}>Add new cabin</Button>
+        {showForm && <CreateCabinForm/>}
     </Row>
     </>
   );
